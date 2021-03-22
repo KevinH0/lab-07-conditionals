7b663946b9bd3d823e384f8a3b40d4f610ef8def
Kevin Huang <striderster@Kevins-MacBook-Air.local>
2021-03-22 16:19:20 -0400

    Basic cake.py added

---
+++ cake.py
@@ -0,0 +1,7 @@
+cake = input("Do you want cake?(yes/no)")
+if(cake == "yes" or cake == "YES"):
+	print("Have some cake")
+elif (cake == "no" or cake == "NO"):
+	print("No cake for you")
+else:
+	print("Invalid Command(yes/no)")
